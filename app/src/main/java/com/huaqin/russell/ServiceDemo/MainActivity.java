5de8d4cf330e6f662b8b1d71bc915cc578ed37f3
Russell Guo <russell.grey@gmail.com>
2018-04-19 17:20:48 +0800

    avoid start background thread if menu not loaded in.
    .

--- app/src/main/java/com/huaqin/russell/ServiceDemo/MainActivity.java
+++ app/src/main/java/com/huaqin/russell/ServiceDemo/MainActivity.java
@@ -179,6 +179,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         mMenu = menu;
+        mButton.setEnabled(true);
         return true;
     }
 
